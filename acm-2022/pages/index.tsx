3c648bd53e63565169a218e4e51e0f7776bad7c6
unknown <bennie.ben095@gmail.com>
2022-08-06 13:52:57 -0700

    Smooth-scroll

--- acm-2022/pages/index.tsx
+++ acm-2022/pages/index.tsx
@@ -8,20 +8,18 @@ import PageOne from '../components/Pages/PageOne/PageOne';
 import PageTwo from '../components/Pages/PageTwo/PageTwo';
 import PageThree from '../components/Pages/PageThree/PageThree';
 import PageFour from '../components/Pages/PageFour/PageFour';
+import PageContainer from '../components/Pages/PageContainer/PageContainer';
+import { ReactElement } from 'react';
 
 
 const Home: NextPage = () => {
+    const pages:ReactElement[] = [<PageOne/>, <PageTwo/>, <PageThree/>, <PageFour/>]
     return (
         <>
-        <div className={`${styles.container} fixed`}>
+        <div className={`${styles.container} fixed z-10`}>
             <Test />
         </div>
-        <div className="h-screen w-full overflow-hidden scroll-smooth">
-            <PageOne/>
-            <PageTwo/>
-            <PageThree/>
-            <PageFour/>
-        </div>
+        <PageContainer pages={pages}/>
         </>
     );
 };
