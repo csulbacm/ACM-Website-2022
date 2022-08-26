f01cdf00dbc62d625f7a45584b853299b89f691e
Bundy-Mundi <bennie.ben095@gmail.com>
2022-08-26 11:33:03 -0700

    Social Media

--- acm-2022/components/LandingPage/index.tsx
+++ acm-2022/components/LandingPage/index.tsx
@@ -3,18 +3,17 @@ import SocialMedia from "./socialMedia";
 const LandingPage = () => {
     const backgroundUrl = "./bg.jpeg";
     return(
+        <>
         <div style={{ 
-            backgroundImage: `url(${backgroundUrl})` 
+            color: "white",
+            background: `rgba(0,0,0,0.45) url(${backgroundUrl})`,
+            backgroundSize: "cover"
+            }} className="min-h-screen w-full bg-no-repeat flex-col flex items-center justify-center bg-filter">
             
-            }} className="min-h-screen w-full bg-no-repeat bg-cover flex-col flex items-center justify-center">
-            
-            <div className="flex-col flex items-center justify-center w-96 break-all mb-10">
-                <Typography variant="h1" className="my-12">ACM</Typography>
+            <div className="flex-col flex items-center justify-center w-96 mb-10">
+                <Typography variant="h1" className="my-12">(Title goes here)</Typography>
                 <Typography variant="lead">
-                Material Tailwind is an easy to use components library for Tailwind CSS
-                and Material Design. It provides a simple way to customize your
-                components, you can change the colors, fonts, breakpoints and everything
-                you need.
+                Welcome to the Association for Computing Machinery at California State University, Long Beach! We are a community of diverse students brought together by our passion for computer science.
                 </Typography>
             </div>
 
@@ -23,6 +22,7 @@ const LandingPage = () => {
                 <SocialMedia/>
             </div>
         </div>
+        </>
     );
 }
 
--- acm-2022/components/LandingPage/socialMedia.tsx
+++ acm-2022/components/LandingPage/socialMedia.tsx
@@ -1,16 +1,22 @@
 import { IconButton } from "@material-tailwind/react";
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiscord, faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const SocialMedia = () => {
-    const social = ["https://www.facebook.com/csulbacm","https://www.instagram.com/csulbacm/?hl=en", "https://discord.com/invite/csulbacm","https://twitter.com/csulbacm?lang=en","https://www.youtube.com/channel/UCbohZYGUm6qNFjnTbLhTHag/featured" ];
-    
-    const ListItem = ({url}:any): any => {
+    const social = [
+        {url: "https://www.facebook.com/csulbacm", icon: faFacebook},
+        {url: "https://www.instagram.com/csulbacm/?hl=en", icon: faInstagram},
+        {url: "https://discord.com/invite/csulbacm", icon: faDiscord},
+        {url: "https://twitter.com/csulbacm?lang=en", icon: faTwitter},
+        {url: "https://www.youtube.com/channel/UCbohZYGUm6qNFjnTbLhTHag/featured" , icon: faYoutube}
+    ];
+    const iconColor = "#FFA726";
+    const ListItem = ({url, icon}:any): any => {
         return(
             <li className="flex items-center justify-center">
                 <a href={url}>
-                    <IconButton variant="outlined">
-                        <i className="fas fa-heart"/>
+                    <IconButton size="lg" variant="outlined">
+                        <FontAwesomeIcon icon={icon} color={iconColor}/>
                     </IconButton>
                 </a>
             </li>
@@ -20,7 +26,7 @@ const SocialMedia = () => {
     return (
         <ul className="grid grid-cols-5 gap-1 w-96">
             {
-                social.map((v, k) => <ListItem url = {v} key = {k}/>)
+                social.map((v, k) => <ListItem url = {v.url} icon={v.icon} key = {k}/>)
             }
         </ul>
     );
