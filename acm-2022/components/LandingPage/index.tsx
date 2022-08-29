b1cf90b5093fcf452dd11e52afb3b5a8f514feed
unknown <bennie.ben095@gmail.com>
2022-08-29 14:03:14 -0700

    Added responsive design

--- acm-2022/components/LandingPage/index.tsx
+++ acm-2022/components/LandingPage/index.tsx
@@ -5,30 +5,37 @@ const LandingPage = () => {
     const backgroundUrl = "./bg.jpeg";
     return(
         <>
-        <div id="particles-container" style={{height: "60vh", width:"100%", overflow:"hidden"}}>
+        <div 
+            id="particles-container"
+            className="h-screen lg:h-70"
+            style={{ width:"100%", overflow:"hidden"}}>
+            
             <Banner/>
-            <div style={{
-                top: "30%",
+            
+            <div id="title__container" style={{
                 left: "50%",
                 transform: "translate(-50%, -50%)"
-            }} className="absolute flex-col flex items-start justify-start lg:items-center lg:justify-center w-auto text-white">
-                <Typography variant="h1" className="my-12">(Title goes here)</Typography>
-                <Typography variant="lead">
-                    Welcome to the Association for Computing Machinery (ACM)
-                </Typography>
-                <Typography variant="lead">
-                    @California State University, Long Beach!
-                </Typography>
-                <Typography>
-                    We are a community of diverse students brought together by our passion for computer science.
-                </Typography>
-                <Typography>
-                    The ACM at CSULB is the largest computer science student organization in CSULB. 
-                </Typography>
+            }} className="relative flex-col flex items-center justify-center w-auto text-white text-xs top-[40%] lg:top-[30%]">
+                <Typography variant="h1" className="">ACM</Typography>
+                <div id="desktop_contents__container" style={{
+
+                }} className="flex flex-col items-center justify-center w-auto text-white text-xs p-6">
+                    <Typography variant="small" className="text-center">
+                        Welcome to the Association for Computing Machinery (ACM)
+                    </Typography>
+                    <Typography variant="small" className="text-center">
+                        @California State University, Long Beach!
+                    </Typography>
+                    <Typography variant="small" className="text-center">
+                        The ACM is the largest computer science student organization in CSULB. 
+                    </Typography>
+                </div>
+            
             </div>
-            <div className="w-full flex-col flex items-center justify-center absolute text-white"
+        
+            <div className="w-full flex-col flex items-center justify-center absolute text-white "
                 style={{
-                    bottom: "5%",
+                    bottom:"0%",
                     left: "50%",
                     transform: "translate(-50%, -50%)"
                 }} >
