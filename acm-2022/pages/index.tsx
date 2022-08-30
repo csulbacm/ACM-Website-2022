4031e9906f1436afbe3e4245ac7b60b623b9df6a
unknown <bennie.ben095@gmail.com>
2022-08-29 17:07:19 -0700

    Small change

--- acm-2022/pages/index.tsx
+++ acm-2022/pages/index.tsx
@@ -3,25 +3,18 @@ import styles from '../styles/Home.module.css';
 import Test from '../components/Navbar/Navbar';
 import Sponsors from '../components/Sponsors/Sponsors';
 import Officers from '../components/Officers/Officers';
-<<<<<<< HEAD
 import Socials from '../components/Socials/Socials';
 import Footer from '../components/Footer/Footer';
 import About from '../components/About/About';
-=======
 import LandingPage from '../components/LandingPage';
->>>>>>> ui/pages
 
 const Home: NextPage = () => {
     return (
         <>
             <div className={styles.main}>
                 <Test />
-<<<<<<< HEAD
                 <About />
-=======
                 <LandingPage/>
-                <Sponsors />
->>>>>>> ui/pages
                 <hr />
                 <Officers />
                 <hr />
